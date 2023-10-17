4ce6ef2315568382be3fad7aa92bc978e1e5b608
Rohit <Rairohit385@gmail.com>
2023-10-17 21:59:56 +0530

    Post Form created using React useForm added slug

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
 import Container from "./container/container";
-import LogoutBtn from "./Header/LogoutBtn";
-
-export {Header,Footer,Container,LogoutBtn};
+import LogoutBtn from "./LogoutBtn";
+import RTE from './RTE'
+export {Header,Footer,Container,LogoutBtn,RTE};
